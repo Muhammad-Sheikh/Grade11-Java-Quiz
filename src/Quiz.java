089839f3dfd0cb787ad9302420b44a8191fbbc57
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2023-05-05 11:10:15 -0400

    Added questions and finished randomize.

--- src/Quiz.java
+++ src/Quiz.java
@@ -25,17 +25,16 @@ public class Quiz {
     boolean gameStart;
 public  void test()
 {
+    questionRandomizer();
+    System.out.println(questions.getQuestion());
+
+
     Scanner userInput = new Scanner(System.in);
 
     int inp = userInput.nextInt();
 
 
-    questionRandomizer();
-
-
-    System.out.println(questions.getQuestion1());
-
-    if(questions.checkAnswer1(inp))
+    if(questions.checkAnswer(inp))
     {
         System.out.println("Correct!");
     } else
@@ -45,41 +44,52 @@ public  void test()
 }
     public void questionRandomizer()
     {
-        int  currentRand;
-        currentRand = 1;
+        double currentRand;
         double currentvalidPool [] = {1, 2 ,3, 4, 5};
-        for (int i = 0; i <= 4; i++)
-        {
 
-          //currentRand = Math.random() * (5 - 1 + 1) + 1;
+          currentRand = Math.random() * (4 - 1 + 1) + 1;
 
+            if(currentvalidPool[(int) currentRand] == 1)
+            {
+                currentvalidPool[(int) currentRand] = 0;
+                questions.setQuestion("What does KAY/O stand for?");
+                questions.setAnswer("Kill All Your Opponents");
+                questions.setOptions(new String[]{"Kill All Your Opponents", "Nothing", "Knock Out!", "Keep All Your 0's"});
+            }
+
+          if(currentvalidPool[(int) currentRand] == 2)
+          {
               currentvalidPool[(int) currentRand] = 0;
-              questions.setQuestion1("What does KAY/O stand for?");
-              questions.setAnswer1("Kill All Your Opponents");
-              questions.setOptions1(new String[] {"Kill All Your Opponents", "Nothing", "Knock Out!", "Keep All Your 0's"});
-
-
-          if(currentvalidPool[(int) currentRand] == 2)
-          {
-
+              questions.setQuestion("Who developed & published Valorant??");
+              questions.setAnswer("Riot Games");
+              questions.setOptions(new String[]{"Rockstar Games", "Ubisoft", "Electronic Arts", "Riot Games"});
           }
 
-          if(currentvalidPool[(int) currentRand] == 2)
+          if(currentvalidPool[(int) currentRand] == 3)
           {
-
+              currentvalidPool[(int) currentRand] = 0;
+              questions.setQuestion("The ability  \"Paranoia\" is used by what agent?");
+              questions.setAnswer("Omen");
+              questions.setOptions(new String[]{"Fade", "Omen", "Brimstone", "Gecko"});
           }
 
-          if(currentvalidPool[(int) currentRand] == 2)
+          if(currentvalidPool[(int) currentRand] == 4)
           {
-
+              currentvalidPool[(int) currentRand] = 0;
+              questions.setQuestion("Which of these are NOT maps in Valorant?");
+              questions.setAnswer("Clock");
+              questions.setOptions(new String[]{"Fracture", "Ascent", "Split", "House"});
           }
 
-          if(currentvalidPool[(int) currentRand] == 2)
+          if(currentvalidPool[(int) currentRand] == 5)
           {
-
+              currentvalidPool[(int) currentRand] = 0;
+              questions.setQuestion("The \"Vandal\" weapon is modeled after what real life gun?");
+              questions.setAnswer("Ak-47");
+              questions.setOptions(new String[]{"Ak-47", "M4 Assault Rifle", "MP5 Parrebluem", "Glock-19"});
           }
-        }
     }
+
 
     public void textClear() {
         for (int row = 1; row <= 21; row = row + 1) {
@@ -89,7 +99,8 @@ public  void test()
         }
     }
 
-    public void tempStop(int seconds) {
+    public void tempStop(int seconds)
+    {
         int milliseconds;
         milliseconds = seconds * 1000;
         try {
@@ -97,6 +108,7 @@ public  void test()
         } catch (InterruptedException ie) {
         }
     }
+
 public boolean titleScreen()
 {
     System.out.println("TYPE IN THE OPTION YOU WANT!");
@@ -122,36 +134,30 @@ public boolean titleScreen()
 
 class Questions
 {
-    private String Answer1, Answer2, Answer3, Answer4, Answer5;
+    private String Answer;
 
-    private String Question1, Question2, Question3, Question4, Question5;
+    private String Question;
 
-    private String Question1Options[];
+    private String QuestionOptions[];
 
-
-    public String getQuestion1 ()
+    public String getQuestion ()
     {
-        return Question1;
+        return Question;
     }
-    public boolean checkAnswer1(int toCheck) {
-        return Objects.equals(Answer1, Question1Options[toCheck]);
+    //public String getOptions
+    public boolean checkAnswer(int toCheck) { return Objects.equals(Answer, QuestionOptions[toCheck]);}
+    public void setAnswer(String newAnswer) {
+        Answer = newAnswer;
     }
-    public void setAnswer1(String newAnswer) {
-        Answer1 = newAnswer;
-    }
-
-    public  void setOptions1(String [] question1Options)
+    public  void setOptions(String [] question1Options)
     {
-        Question1Options = question1Options.clone();
+        QuestionOptions = question1Options.clone();
     }
-
-    public void setQuestion1(String newQuestion) {
-        Question1 = newQuestion;
+    public void setQuestion(String newQuestion) {
+        Question = newQuestion;
     }
 
 }
-
-
 
 }
 
